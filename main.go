e53a4e261f0c7e0a8b0d15f603067fab57e9e803
yamaguti-Hikaru <hikaru171102@icloud.com>
2026-03-02 22:31:56 +0900

    Update todo

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	_"fmt"
 	//"log"
 	//"go.todo_app/config"
 	"go.todo_app/app/models"
@@ -16,18 +16,17 @@ func main() {
 
 		log.Println("Test")
 	*/
-
+/*
 	fmt.Println(models.Db)
-	
-		u := &models.User{}
-		u.Name = "test2"
-		u.Email = "test2@example.com"
-		u.Password = "testtest"
-		fmt.Println(u)
 
-		u.CreateUser()
-		
+	u := &models.User{}
+	u.Name = "test2"
+	u.Email = "test2@example.com"
+	u.Password = "testtest"
+	fmt.Println(u)
 
+	u.CreateUser()
+*/
 	/*
 		u, _ := models.GetUser(1)
 		fmt.Println(u)
@@ -51,19 +50,25 @@ func main() {
 	   t, _ := models.GetTodo(1)
 	   fmt.Println(t)
 	*/
+	/*
 	user, _ := models.GetUser(3)
 	user.CreatedTodo("Third Todo")
+	*/
 	/*
-	todos, _ := models.GetTodos()
-	for _, v := range todos {
-		fmt.Println(v)
-	}
+		todos, _ := models.GetTodos()
+		for _, v := range todos {
+			fmt.Println(v)
+		}
+	*/
+	/*
+		user2, _ := models.GetUser(2)
+		todos, _ := user2.GetTodosByUser()
+		for _, v := range todos {
+			fmt.Println(v)
+		}
 	*/
 
-	user2, _ := models.GetUser(2)
-	todos, _ := user2.GetTodosByUser()
-	for _, v := range todos {
-		fmt.Println(v)
-	}
-
+	  t, _ := models.GetTodo(1)
+	   t.Content = "Updated Todo"
+	   t.UpdateTodo()
 }
