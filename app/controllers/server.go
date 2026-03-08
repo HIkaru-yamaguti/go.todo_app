58e998ffdc1c21c86fa2be310cbfddd6a8a1377a
yamaguti-Hikaru <hikaru171102@icloud.com>
2026-03-08 20:44:32 +0900

    layout

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -1,10 +1,22 @@
 package controllers
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
 
 	"go.todo_app/config"
 )
+
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _ , file := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
+	}
+
+	template := template.Must(template.ParseFiles(files...))
+	template.ExecuteTemplate(w, "layout", data)
+}
 
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
